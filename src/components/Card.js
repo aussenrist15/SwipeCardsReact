1e4721858195137a023625225c8f714a60f41167
Abdul Basit <chabdulbasit1122@gmail.com>
2020-12-15 13:00:04 +0500

    aa

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,25 +10,20 @@ const Card = ({ i, x, y, rot, scale, trans, bind, data }) => {
     <animated.div
       key={i}
       style={{
-        transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`)
+        transform: interpolate(
+          [x, y],
+          (x, y) => `translate3d(${x}px,${y}px,0)`
+        ),
       }}
     >
       <animated.div
         {...bind(i)}
         style={{
-          transform: interpolate([rot, scale], trans)
+          transform: interpolate([rot, scale], trans),
         }}
       >
         <div className="card">
-          <Carousel>
-            {pics.map((pic, index) => (
-              <img src={pic} key={index} alt="profilePicture" />
-            ))}
-          </Carousel>
-          <h2>{name},</h2>
-          <h2>{age}</h2>
-          <h5>{distance}</h5>
-          <h5>{text}</h5>
+          <img src={pics} alt="profilePicture" />
         </div>
       </animated.div>
     </animated.div>
@@ -40,7 +35,7 @@ Card.propTypes = {
   age: number,
   distance: string,
   text: string,
-  pics: array
+  pics: array,
 };
 
 export default Card;
